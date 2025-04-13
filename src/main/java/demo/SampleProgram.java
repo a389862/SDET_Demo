d01157f519a046214afcd35e5403457f86ed0390
Hari Prasad <r.hariprasad@gmail.com>
2025-04-13 17:20:43 +0530

    added 3rd parameter

--- src/main/java/demo/SampleProgram.java
+++ src/main/java/demo/SampleProgram.java
@@ -5,6 +5,7 @@ public class SampleProgram {
 	public static void main(String[] args) {
 		int x = 10;
 		int y = 20;
+		//added another parameter for addition
 		int z = 30;
 		System.out.println(x+y+z);
 
